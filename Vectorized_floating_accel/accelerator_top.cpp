c6320c006d1ae3f19da8fb2bdc45da10cec9f54e
Prahalad Chari <pchar4@gmail.com>
2025-02-21 11:34:04 -0600

    Needs to include prints but could theoretically work

--- Vectorized_floating_accel/accelerator_top.cpp
+++ Vectorized_floating_accel/accelerator_top.cpp
@@ -1,29 +1,39 @@
 #include "layer.h"
 #include "activations.h"
+#include "error.h"
 #include <iostream>
 #include <iomanip>  // for std::setw (nice formatting)
 #include <vector>
 #include <cmath>
 
-std::vector<std::vector<double>> weights = {{0, 0}, {0, 0}};
-std::vector<double> biases = {0, 0};
-std::vector<std::vector<double>> input = {{0}, {1}};
+
+int main(){
+    std::vector<std::vector<double>> weights = {{0, 0}, {0, 0}};
+    std::vector<double> biases = {0, 0};
+    std::vector<std::vector<double>> input = {{0.0}, {1.0}};
+    std::vector<std::vector<double>> y_true = {{1}};
+    std::vector<std::vector<double>> copy;
+    // Dummy activation parameter
+    int activation = 0;
+
+    // =========== FORWARD PROPAGATION ===========
+    std::vector<std::vector<double>> result = forwardPropagation(input, weights, biases, activation);
+
+    double categorical_cross_entropy = categoricalCrossEntropy(y_true, result);
 
 
-// Dummy activation parameter
-int activation = 0;
+    std::vector<std::vector<double>> final_error_vector = {{result[0][0] - y_true[0][0]}};
 
-// =========== FORWARD PROPAGATION ===========
-std::vector<std::vector<double>> result = forwardPropagation(input, weights, biases, activation);
 
-std::vector<std::vector<double>> categorical_cross_entropy;
 
-std::vector<std::vector<double>> backPropagationSingleSample(
-    input,
-    weights,
-    biases,
-    result,
-    std::vector<std::vector<double>>& dOut,
-    int activation, // if you need to handle other activation types
-    int learning_rate
-)
+    std::vector<std::vector<double>> d_minus1 = backPropagationSingleSample(
+        input,
+        weights,
+        biases,
+        result,
+        final_error_vector,
+        0, // if you need to handle other activation types
+        0.1
+    );
+    return 0;
+}
