d3d6584aff497443c8c38b77de8968b493818aec
Prahalad Chari <pchar4@gmail.com>
2025-02-22 23:50:21 -0600

    Implements top level changes, now to focus on debugging update temp mat and weights mat size mismatch

--- Vectorized_floating_accel/activations.h
+++ Vectorized_floating_accel/activations.h
@@ -7,6 +7,7 @@
 
 // ReLU activation function
 std::vector<double> relu(std::vector<double>& net);
+std::vector<double> derivative_relu(std::vector<double>& net);
 
 // Sigmoid activation function
 std::vector<double> sigmoid(std::vector<double>& net);
