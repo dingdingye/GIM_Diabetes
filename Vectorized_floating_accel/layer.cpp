95b89246498ff08e01d76c0b83957eab18b54e65
Prahalad Chari <pchar4@gmail.com>
2025-02-22 18:32:43 -0600

    starts on implementing a new backprop alg

--- Vectorized_floating_accel/layer.cpp
+++ Vectorized_floating_accel/layer.cpp
@@ -24,7 +24,7 @@ void print1D(const std::vector<double>& vec) {
     }
 }
 
-std::vector<std::vector<double> > transpose(std::vector<std::vector<double> >& matrix) {
+std::vector<std::vector<double>> transpose(std::vector<std::vector<double> >& matrix) {
 
     // Create a new matrix with dimensions flipped (cols x rows).
     // Number of rows and columns of the original matrix.
@@ -168,6 +168,21 @@ std::vector<std::vector<double> > forwardPropagation(
     }
     
     return output;
+}
+
+std::vector<std::vector<double>> backProp(
+    std::vector<std::vector<double>> w_l_plus1,
+    std::vector<std::vector<double>> d_l_plus1,
+    int activation
+)
+{
+    std::vector<std::vector<double>> d_l(d_l_plus1.size(), std::vector<double>(d_l_plus1[0].size(), 0.0));
+    std::vector<std::vector<double>> w_l_plus1_T = transpose(w_l_plus1);
+    std::vector<std::vector<double>> temp = matmul(w_l_plus1_T, d_l_plus1);
+    
+    if (activation == 0) {
+        
+    }
 }
 
 std::vector<std::vector<double> > backPropagationSingleSample(
